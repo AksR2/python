10c8c57ccc8b476c13cd7eff23e9a407f6471933
AksR2 <Akshay.924@outlook.com>
2014-02-24 18:20:17 +0530

    hackerrank feb20

---
+++ hck/hck1-feb.py
@@ -0,0 +1,16 @@
+i=0
+
+a=[]
+b=[]
+a=raw_input()
+b=a.split(" ")
+for i in range(0,len(b)):
+	b[i]=int(b[i])
+
+for i in range(b[0]):
+	a[i]=input()
+q=[]
+for i in range(b[2])
+	q[i]=input()
+	if q[i]<=b[1]:
+		print a[b[0]-q[i]-b[1]]
